7578df525c5b2a126167e3557d7ad5495f8cae5f
dong chan <sts05147@naver.com>
2025-03-26 15:05:16 +0900

    GuestDashboard UI 완료했습니다. 졸라 힘드네요 하암

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -53,6 +53,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation ("androidx.compose.material:material-icons-extended:1.7.8")
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
